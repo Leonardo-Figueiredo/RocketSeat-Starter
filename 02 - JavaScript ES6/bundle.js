f680dd31db00054a0b9e863d9e1ad9146941898f
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2019-12-06 14:06:55 -0300

    Exercício 04 - ES6

--- 02 - JavaScript ES6/bundle.js
+++ 02 - JavaScript ES6/bundle.js
@@ -1,40 +1,76 @@
 "use strict";
 
-// 3.1
-var arr = [1, 2, 3, 4, 5];
-arr.map(function (item) {
-  return item + 10;
-});
-console.log(arr); // 3.2
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var empresa = {
+  nome: 'Rocketseat',
+  endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC'
+  }
+}; //4.1
+
+var nome = empresa.nome,
+    _empresa$endereco = empresa.endereco,
+    cidade = _empresa$endereco.cidade,
+    estado = _empresa$endereco.estado;
+console.log(nome);
+console.log(cidade);
+console.log(estado); //4.2
+
+function mostraInfo(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  return "".concat(nome, " tem ").concat(idade, " anos.");
+}
+
+console.log(mostraInfo({
+  nome: 'Diego',
+  idade: 23
+})); //5.1
+
+var arr = [1, 2, 3, 4, 5, 6];
+var x = arr[0],
+    y = arr.slice(1);
+console.log(x);
+console.log(y);
+
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
+}
+
+console.log(soma(1, 2, 3, 4, 5, 6, 7, 8, 9, 500)); //5.2
 
 var usuario = {
   nome: 'Diego',
-  idade: 23
+  idade: 23,
+  endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil'
+  }
 };
 
-var mostraIdade = function mostraIdade(usuario) {
-  return usuario.idade;
-};
+var usuario2 = _objectSpread({}, usuario, {
+  nome: 'Gabriel'
+});
 
-console.log(mostraIdade(usuario)); // 3.3
+var usuario3 = _objectSpread({}, usuario, {
+  endereco: _objectSpread({}, usuario.endereco, {
+    cidade: 'Lontras'
+  })
+});
 
-var nome = "Diego";
-var idade = 23;
-
-var mostraUsuario = function mostraUsuario() {
-  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Diego';
-  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
-  return {
-    nome: nome,
-    idade: idade
-  };
-};
-
-console.log(mostraUsuario(nome, idade));
-console.log(mostraUsuario(nome)); // 3.4
-
-var promise = function promise() {
-  return new Promise(function (resolve, reject) {
-    return resolve();
-  });
-};
+console.log(usuario);
+console.log(usuario2);
+console.log(usuario3);
--- 02 - JavaScript ES6/main.js
+++ 02 - JavaScript ES6/main.js
@@ -1,19 +1,54 @@
-// 3.1
-const arr = [1, 2, 3, 4, 5];
-arr.map(item => item + 10);
-console.log(arr);
+const empresa = {
+  nome: 'Rocketseat',
+  endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC',
+  }
+ };
 
-// 3.2
-const usuario = { nome: 'Diego', idade: 23 };
-const mostraIdade = usuario => usuario.idade;
-console.log(mostraIdade(usuario));
+ //4.1
+ const { 
+  nome,
+  endereco: { cidade, estado } 
+} = empresa;
 
-// 3.3
-const nome = "Diego";
-const idade = 23;
-const mostraUsuario = (nome = 'Diego', idade = 18) => ({ nome, idade });
-console.log(mostraUsuario(nome, idade));
-console.log(mostraUsuario(nome));
+ console.log(nome);
+ console.log(cidade);
+ console.log(estado);
 
-// 3.4
-const promise = () => new Promise((resolve, reject) => resolve());
+ //4.2
+function mostraInfo({ nome, idade }) {
+  return `${nome} tem ${idade} anos.`;
+}
+console.log(mostraInfo({ nome: 'Diego', idade: 23 }));
+
+//5.1
+const arr = [1, 2, 3, 4, 5, 6];
+const [ x, ...y ] = arr;
+console.log(x);
+console.log(y);
+
+function soma(...params) {
+  return params.reduce((total, next) => total + next);
+}
+console.log(soma(1,2,3,4,5,6,7,8,9,500));
+
+//5.2
+const usuario = {
+  nome: 'Diego',
+  idade: 23,
+  endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil',
+  }
+};
+const usuario2 = { ...usuario, nome:'Gabriel' };
+const usuario3 = { 
+  ...usuario,
+  endereco: {...usuario.endereco, cidade:'Lontras' } 
+};
+
+console.log(usuario);
+console.log(usuario2);
+console.log(usuario3);
