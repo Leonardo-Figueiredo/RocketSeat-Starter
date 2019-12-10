8873b7bbeedacde55a8868165ab0f3897e19a077
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2019-12-10 00:00:08 -0300

    Exercício 01 - Webpack Server

---
+++ 02 - JavaScript ES6/src/main.js
@@ -0,0 +1,10 @@
+// 1.1 
+// import ClasseUsuario, { idade } from './functions';
+// ClasseUsuario.info();
+
+// 1.2
+// alert(idade);
+
+// 1.3
+import ClasseUsuario, { idade as IdadeUsuario} from './functions';
+console.log(IdadeUsuario);
