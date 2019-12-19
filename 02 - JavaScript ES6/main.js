0af42e46760390ac4a0f0fc003b40595b7c2a9c8
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2019-12-19 14:15:58 -0300

    MongoDB, First Route in NodeJS

--- 02 - JavaScript ES6/main.js
+++
@@ -1,9 +0,0 @@
-import * as functions from './functions';
-// import somaFunction from './soma';
-
-console.log(functions.soma(1, 2));
-console.log(functions.mult(4, 3));
-console.log(functions.sub(4, 3));
-
-// console.log(soma(1,2));
-// console.log(sub(4,2));
